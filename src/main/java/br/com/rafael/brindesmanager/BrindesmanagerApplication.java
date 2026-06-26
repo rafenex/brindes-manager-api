f7eaef0cd343ce99d6da6ecda2e5a9b33f5a9429
rafenex <dvr.rafa@gmail.com>
2026-06-26 13:15:41 -0300

    chore: create Spring Boot project base

---
+++ src/main/java/br/com/rafael/brindesmanager/BrindesmanagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafael.brindesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrindesmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrindesmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rafael/brindesmanager/BrindesmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafael.brindesmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrindesmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
