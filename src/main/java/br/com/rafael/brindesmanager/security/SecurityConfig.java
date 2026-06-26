0f62d5a99a147aaf2138d6a721c0de128d0a7014
rafenex <dvr.rafa@gmail.com>
2026-06-26 15:20:20 -0300

    chore: add docker support for api and database

--- src/main/java/br/com/rafael/brindesmanager/security/SecurityConfig.java
+++ src/main/java/br/com/rafael/brindesmanager/security/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/api/auth/register",
                                 "/api/auth/login",
-                                "/actuator/health",
+                                "/actuator/**",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html"
